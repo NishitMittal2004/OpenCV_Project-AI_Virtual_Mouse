9c3f34cf44ef48a0400e00f54825c885ede5b87d
Nishit Mittal <123050996+NishitMittal2004@users.noreply.github.com>
2023-07-30 13:55:49 +0530

    Add files via upload

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+import math
 
 
 class handDetector():
@@ -16,6 +17,8 @@ class handDetector():
                                         self.detectionCon,self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
+        self.tipIds = [4, 8, 12, 16, 20]
+
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
@@ -28,9 +31,12 @@ class handDetector():
                                                self.mpHands.HAND_CONNECTIONS)
         return img
 
-    def findPosition(self, img, handNo=0, draw=True):
 
-        lmlist = []
+    def findPosition(self, img, handNo=0, draw=True):
+        xList = []
+        yList = []
+        bbox = []
+        self.lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
 
@@ -38,12 +44,63 @@ class handDetector():
                 # print(id,lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
                 # print(id, cx, cy)
-                lmlist.append((id, cx, cy))
+                self.lmList.append((id, cx, cy))
                 if draw:
-                    cv2.circle(img, (cx, cy), 7, (255,0,255), cv2.FILLED)
+                    cv2.circle(img, (cx, cy), 4, (255,0,255), cv2.FILLED)
 
-        return lmlist
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            bbox = xmin, ymin, xmax, ymax
+
+            if draw:
+                cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
+                              (0,255,255), 3)
+
+        return self.lmList, bbox
+
+
+    def fingersUp(self):
+        fingers = []
+
+        # Thumb
+        x1, y1 = self.lmList[4][1], self.lmList[4][2]
+        x2, y2 = self.lmList[9][1], self.lmList[9][2]
+        length = math.hypot(x2 - x1, y2 - y1)
+        # print(int(length))
+        if length > 50:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # Other 4 Fingers
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
+
+
+    def findDistance(self, p1, p2, img, draw=True, r=10, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (0, 0 ,255), t)
+            cv2.circle(img, (x1, y1), r, (0, 0 ,255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 -x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
+
+
+
+
 def main():
     pTime = 0
     cTime = 0
@@ -52,9 +109,10 @@ def main():
     while True:
         success, img = cap.read()
         img = detector.findHands(img)
-        lmlist = detector.findPosition(img)
-        if len(lmlist) != 0:
-            print(lmlist[4])
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
+
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
